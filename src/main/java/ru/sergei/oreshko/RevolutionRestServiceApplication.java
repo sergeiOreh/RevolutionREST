589304d6ad3ba1b7dbaff93e68e79a794a1fbe4b
Sergei <oreshko-sergei@mail.ru>
2020-10-23 20:38:10 +0300

    LF replaced by CRLF

---
+++ src/main/java/ru/sergei/oreshko/RevolutionRestServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.sergei.oreshko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolutionRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevolutionRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sergei/oreshko/RevolutionRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergei.oreshko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevolutionRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
